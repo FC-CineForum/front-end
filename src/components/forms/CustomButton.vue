d16e1d0a00e7ee4fc4fbee959f79a3f605c7b11b
UrielLoominer <u.luna@sienetworks.com>
2022-04-19 19:07:00 -0500

    :sparkles: Componente boton

---
+++ src/components/forms/CustomButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button>
+        <slot />   
+    </button>
+</template>
+
+<style scoped>
+button {
+  padding: 10px;
+  border: none;
+  color: white;
+  background-color: var(--color-background-main);
+}
+</style>
