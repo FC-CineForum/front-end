43f9da2ed4d31ce33ba1563fade58eb40516ff1b
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel.local>
2022-05-04 19:30:43 -0500

    :construction: Video and image display

--- src/components/async/EntryDisplay.vue
+++ src/components/async/EntryDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mt-5 mx-2 d-flex justify-content-between">
+    <div class="mt-5 mx-5 d-flex flex-row justify-content-between">
         <div class="d-flex flex-column">
             <h1 class="h1 fw-bold">{{info.title}}</h1>
             <div class="d-flex fs-3 stats">
@@ -21,11 +21,42 @@
             </div>
         </div>
     </div>
+    <div class="row d-flex flex-row justify-content-center">
+        <div class="col-12 col-md-10 d-flex flex-column flex-md-row justify-content-center  align-items-center align-items-md-end">
+            <img :src="info.poster" alt="poster">
+            <iframe :src="info.trailer" @click="changeVideoStatus"></iframe>
+        </div>
+    </div>
+    <div class="mt-4 row d-flex flex-row justify-content-center">
+        <div class="col-12 col-md-10 d-flex flex-row">
+            <div class="rounded-pill p-1 gender d-flex flex-row justify-content-center">
+            <span>Ciencia ficción</span>
+        </div>
+        <div class="rounded-pill p-1 gender d-flex flex-row justify-content-center">
+            <span>Acción</span>
+        </div>
+        <div class="rounded-pill p-1 gender d-flex flex-row justify-content-center">
+            <span>Suspenso</span>
+        </div>
+        </div>
+    </div>
+    <div class="mt-4 row d-flex flex-row justify-content-center">
+        <div class="col-12 col-md-10">
+            <h4> <span class="fw-bold"> Director:</span> <span class="info pointer">Lorem Ipsum</span></h4>
+            <h4> <span class="fw-bold"> Guionistas:</span> <span class="info pointer">Lorem Ipsum</span> <span class="info pointer">Lorem Ipsum</span></h4>
+        </div>
+    </div>
+    <div class="mt-4 row d-flex justify-content-center">
+        <div class="col-12 col-md-10">
+            <h4 class="fw-bold">Elenco:</h4>
+            <div class="cast"></div>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import ClapperBoard from "../icons/solids/ClapperBoard.vue";
-import { defineProps } from "vue";
+import { defineProps , ref} from "vue";
 
 const props = defineProps({
     title:{
@@ -38,12 +69,13 @@ const getEntryInfo = async () =>{
     return {
         title: "The batman",
         poster: "https://static.posters.cz/image/1300/posters/the-batman-2022-i122127.jpg",
-        trailer: "https://www.youtube.com/watch?v=fWQrd6cwJ0A",
+        trailer: "https://www.youtube.com/embed/fWQrd6cwJ0A",
         rating: "4.2"
     }
 }
 
 const info = await getEntryInfo()
+
 </script>
 
 <style scoped>
@@ -56,8 +88,45 @@ svg{
     width: 50px;
 }
 
+iframe{
+    width: 100%;
+    height: 500px;
+}
+
+img{
+    
+    width: 250px;
+    height: 350px;
+}
+
 .stats{
     color: rgba(0, 0, 0, .2);
+}
+
+.info{
+    color:var(--cf-main);
+    font-weight: normal;
+}
+
+.gender{
+    border: var(--color-border-main) 2px solid;
+    min-width: 100px;
+    margin-right: 2em;
+}
+
+.cast{
+    width: 100%;
+    max-height: 400px;
+}
+
+gender:last-child{
+    margin-right: 0;
+}
+
+@media (min-width: 768px){
+    img{
+        transform: translateX(50px);
+    }
 }
 
 </style>
