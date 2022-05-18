941cefc70ac5d22f5108bcf4bdbc03311118d4f7
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel.local>
2022-05-17 20:11:50 -0500

    :sparkles: Close dropdown on out click

--- src/components/utilities/Dropdown.vue
+++ src/components/utilities/Dropdown.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <div class="wrapper" @click="changeShowContent" :class="showContent? 'd-inline' : 'd-none'"></div>
         <div @click="changeShowContent">
             <slot name="toggle"/>
         </div>
@@ -29,5 +30,16 @@ const changeShowContent = ()=>{
 .content{
     position: absolute;
     right: 0;
+    z-index: 2;
 }
+.wrapper{
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color:rgba(0, 0, 0, .0);
+  position: fixed;
+  z-index: 1;
+}
+
 </style>
