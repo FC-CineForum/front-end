f4c51b8805ff0b43ac79d48f878471c39185385c
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel.local>
2022-06-03 12:16:05 -0500

    :construction: Entry conections with api

--- src/views/user/HomeView.vue
+++ src/views/user/HomeView.vue
@@ -1,19 +1,13 @@
-<script setup>
-import { reactive } from "vue";
-import Entry from "../../components/Entry.vue";
-import Header from "@/components/Header.vue";
-const movie = reactive({
-  title: "The Batman",
-  rating: 4.2,
-  poster:
-    "https://static.posters.cz/image/1300/posters/the-batman-2022-i122127.jpg",
-});
-</script>
-
 <template>
   <Header />
   <h1>Nuevas Entradas</h1>
   <div class="container-fluid d-flex flex-row justify-start">
-    <Entry :movie="movie" />
   </div>
 </template>
+
+<script setup>
+import { reactive } from "vue";
+import Entry from "../../components/Entry.vue";
+import Header from "@/components/Header.vue";
+
+</script>
