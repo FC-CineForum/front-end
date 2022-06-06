0438be772dfaf2c22cd21ce793a133b5feb752f1
FernandoLB <fernandolb@ciencias.unam.mx>
2022-06-05 22:41:10 -0500

    :sparkles: Adding post/user admin views

---
+++ src/views/admin/UsersAdmin.vue
@@ -0,0 +1,86 @@
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+<Header/>
+<div class="fields">
+    <form action="">
+        <br>
+        <label for="title" class="tag">Username</label>&nbsp
+        <input class="campo" placeholder="Username"/>
+        <br>
+        <br>
+        <input type="submit" class="sbmt" value="Eliminar"/>&nbsp
+        <input type="submit" class="sbmt" value="Admin"/>
+        <br>
+        <br>
+    </form>
+</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  .sbmt {
+      background-color: #083F6D;
+      color: white;
+      width: 4cm;
+      height: 2cm;
+      font-size: 0.7cm;
+      border-width: 0.3cm;
+      border-color: #083F6D;
+      position: relative;
+      left: 10cm;
+  }
+
+  h1 {
+      font-size: 1.5cm;
+      color : #083F6D;
+      text-align: center;
+  }
+
+  .opciones{
+      background-color: #083F6D;
+      color: white;
+      width: 6cm;
+      height: 2cm;
+      font-size: 0.7cm;
+      border-width: 0.3cm;
+      border-color: black;
+      text-align: center;
+  }
+
+  .campo{
+      background-color: #a4c1dde4;
+      color: rgb(2, 1, 1);
+      width: 8cm;
+      height: 2cm;
+      font-size: 0.7cm;
+      border-width: 0.3cm;
+      border-color: #083F6D;
+      text-align: center;
+  }
+
+  .tag {
+      background-color: #083F6D;
+      color: rgb(255, 255, 255);
+      width: 5cm;
+      height: 1cm;
+      font-size: 0.6cm;
+      border-width: 0.3cm;
+      border-color: #083F6D;
+      text-align: center;
+  }
+
+  .fields {
+      background-color: #a4c1dde4;
+      text-align: center;
+  }
+}
+</style>
