8677953a893066bd02f3bcb1e78ca3d034fa167e
FernandoLB <fernandolb@ciencias.unam.mx>
2022-06-05 22:40:28 -0500

    :sparkles: Adding entrie views

---
+++ src/views/admin/AlterEntry.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+    <!--
+    <a class="opciones">Título &nbsp 
+    <input class="campo" placeholder="Título a modificar..."/>
+    </a>
+    <br>
+    <input type="submit" class="opciones" value="Título"> &nbsp
+
+    -->
+    <input class="campo" placeholder="Título a modificar..."/>
+</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  h1 {
+      font-size: 1.5cm;
+      color : #083F6D;
+      text-align: center;
+  }
+
+  .opciones{
+      background-color: #083F6D;
+      color: white;
+      width: 6cm;
+      height: 2cm;
+      font-size: 0.7cm;
+      border-width: 0.3cm;
+      border-color: black;
+      text-align: center;
+  }
+
+  .campo{
+      background-color: #a4c1dde4;
+      color: rgb(2, 1, 1);
+      width: 8cm;
+      height: 2cm;
+      font-size: 0.7cm;
+      border-width: 0.3cm;
+      border-color: #083F6D;
+      text-align: center;
+  }
+}
+</style>
---
+++ src/views/admin/Entries.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { reactive } from "vue";
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+<Header/>
+<div class="center">
+    <h1>Administrador de Entradas</h1>
+
+    <form action="/admin/entries/add">
+        <input type="submit" class="opciones" value="Añadir una Película"/>
+    </form>
+    <form action="/admin/entries/delete">
+        <input type="submit" class="opciones" value="Eliminar una Entrada"/>
+    </form>
+    <form action="/admin/entries/modify">
+        <input type="submit" class="opciones" value="Modificar una Entrada"/>
+    </form>
+    <form action="/admin">
+        <input type="submit" class="opciones" value="Menú Admin"/>
+    </form>
+
+</div>
+</template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  h1 {
+      font-size: 2cm;
+      color : #083F6D;
+      text-align: center;
+      border-width: 1cm;
+  }
+
+  .opciones{
+      background-color: #083F6D;
+      color: white;
+      width: 18cm;
+      height: 5cm;
+      font-size: 1cm;
+      border-width: 1cm;
+      border-color: black;
+      text-align: center;
+      align-items: center;
+  }
+
+  .center {
+      border: 5px solid #083F6D;
+      display: flexbox;
+      justify-content: center;
+      text-align: center;
+      background-color: rgb(140, 190, 202)
+  }
+}
+</style>
