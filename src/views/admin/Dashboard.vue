936dd95e4f19d3631d549e121f652a286fb885e1
FernandoLB <fernandolb@ciencias.unam.mx>
2022-06-05 22:40:05 -0500

    :sparkles: Change dashboard admin

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,13 +1,66 @@
-<template>
-    <div>
-        <h1>Soy la vista del administrador</h1>
-    </div>
-</template>
-
 <script setup>
-
+import Header from "@/components/Header.vue";
 </script>
 
-<style scoped>
+<template>
+  <Header/>
+  <div class="admin">
+    <h1>Menú de Administrador</h1>
 
+    <form action="/admin/entries">
+        <input type="submit" class="opciones" value="Administrador de Entrada"/>
+    </form>
+    <form action="/admin/users">
+      <input type="submit" class="opciones" value="Administrador de Usuarios"/>
+    </form>
+    <form action="/admin/posts">
+      <input type="submit" class="opciones" value="Administrador de Publicaciones"/>
+    </form>
+    <form action="/.">
+      <input type="submit" class="opciones" value="Menú principal"/>
+    </form>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  h1 {
+      font-size: 1.5cm;
+      color : #083F6D;
+      text-align: center;
+  }
+
+  .opciones{
+      background-color: #083F6D;
+      color: white;
+      width: 18cm;
+      height: 5cm;
+      font-size: 1cm;
+      border-width: 1cm;
+      border-color: black;
+      text-align: center;
+  }
+
+  .admin {
+    text-align: center;
+    background-color: rgb(140, 190, 202);
+  }
+  
+  .column {
+      float:left;
+      width: 50%
+  }
+
+  .row:after{
+    display: table;
+    clear: both;
+    content: "";
+  }
+}
 </style>
