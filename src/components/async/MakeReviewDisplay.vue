21d4e2e3e923b52b12646c5b1c191eeeda006c83
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel.local>
2022-06-11 21:14:30 -0500

    :alien: Entry changes

--- src/components/async/MakeReviewDisplay.vue
+++ src/components/async/MakeReviewDisplay.vue
@@ -2,11 +2,11 @@
     <div>
         <div class="m-0 mt-4 container-fluid row d-flex justify-content-center">
             <div class="col-8 col-md-7  col-lg-3">
-                <img :src="entry.image" alt="poster">
+                <img :src="data.entry.image" alt="poster">
             </div>
             <div class="col-8 col-md-7 col-lg-7 d-flex flex-column">
                 <div class="d-flex flex-row justify-content-between">
-                    <h1 class="h1"><span class="fw-bold">{{entry.title}}</span> <span class="fw-light">({{entry.release}})</span></h1>
+                    <h1 class="h1"><span class="fw-bold">{{data.entry.title}}</span> <span class="fw-light">({{data.entry.release}})</span></h1>
                 </div>
             </div>
       </div>
@@ -38,7 +38,7 @@ const fetchData = async () => {
   }
 }
 
-const entry = await fetchData();
+const data = await fetchData();
 
 const review = ref("")
 
