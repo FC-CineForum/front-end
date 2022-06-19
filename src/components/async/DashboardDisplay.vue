9bd508277b6b8ba9134d39bed0211810a5198792
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel.local>
2022-06-19 13:49:04 -0500

    :lipstick: Search bar in dashboard

--- src/components/async/DashboardDisplay.vue
+++ src/components/async/DashboardDisplay.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <div class="my-3 container-fluid d-flex justify-content-end">
+            <div class="p-2 border border-dark rounded-pill d-flex justify-between">
+                <div>
+                    <i class="fas fa-search"></i>
+                    <Input type="text" v-model:value="search" />
+                </div>
+                <div class="pointer" v-if="search">
+                    <i  @click="search = '' " class="fas fa-times"></i>
+                </div>
+            </div>
+        </div>
         <h1>Nuevas Entradas</h1>
         <div class="mt-5 mb-5  overflow-auto" style="white-space: nowrap;">
             <Entry class="me-3 d-inline-block" v-for="entry in entries" :entry="entry" />
@@ -8,23 +19,44 @@
 </template>
 
 <script setup>
+import { reactive, ref, watch } from 'vue';
 import Entry from "@/components/Entry.vue";
+import Input from "@/components/forms/Input.vue";
 import { useDashboardStore } from "@/stores/dashboard.js";
+
+const search = ref("");
 
 const dashboard = useDashboardStore();
 
-const fetchDashboard = async () => {
-    if(dashboard.entries.length >= 1){
-        return dashboard.entries;
+const entries = reactive([])
+
+watch(search,async (newQuestion) => {
+    entries.splice(0,entries.length)
+    if(newQuestion.length  === 0){
+        await fetchDashboard();
+    }else{
+
     }
-    try {
+} )
+
+const fetchDashboard = async () => {
+    entries.splice(0, entries.length);
+    if(dashboard.entries.length >= 1){
+        dashboard.entries.forEach(entry => {
+            entries.push(entry);
+        });
+    }else{
+        try {
         const data = await dashboard.fetchDashboard();
-        return data;
-    } catch (e) {
-        alert(e);
+        data.forEach(entry => {
+            entries.push(entry);
+        });
+        }catch (e) {
+            alert(e);
+        }
     }
 };
 
-const entries = await fetchDashboard();
+ await fetchDashboard();
 
 </script>
